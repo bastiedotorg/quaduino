e52bbe4b60c9e73f6422a05c2460c891d57062d6
Arthur Schmitz <art_schmitz@t-online.de>
2021-09-27 11:23:08 +0200

    Added comments in inconma.cpp

--- src/inconma.cpp
+++ src/inconma.cpp
@@ -5,35 +5,88 @@
 #include "database.h"
 
 
-
+/**
+ *
+ * @param r_max Upper limit for allowed range for value
+ * @param r_min Lower limit for allowed range for value
+ * @param value SINT32 value to be checked
+ * @return true if value within the range and false if its outside
+ */
 bool range_check_sint32(SINT32 r_max, SINT32 r_min, SINT32 value){
     return value >= r_min && value <= r_max;
 }
 
+
+/**
+ *
+ * @param r_max Upper limit for allowed range for value
+ * @param r_min Lower limit for allowed range for value
+ * @param value UINT32 value to be checked
+ * @return true if value within the range and false if its outside
+ */
 bool range_check_uint32(UINT32 r_max, UINT32 r_min, UINT32 value){
     return value >= r_min && value <= r_max;
 }
 
+
+/**
+ *
+ * @param r_max Upper limit for the allowed range for value
+ * @param r_min Lower limit for the allowed range for value
+ * @param value FLOAT64 value to be checked
+ * @return true if value within the range and false if its outside
+ */
 bool range_check_float64(FLOAT64 r_max, FLOAT64 r_min, FLOAT64 value){
     return value >= r_min && value <= r_max;
 }
 
+
+/**
+ *
+ * @param scale_factor SINT32 scale factor (e.g. 3.6 to scale from m/s to km/h)
+ * @param value SINT32 value to be scaled
+ * @return scaled SINT32 value
+ */
 SINT32 scaling_sint32(SINT32 scale_factor, SINT32 value){
     return scale_factor * value;
 }
 
+/**
+ *
+ * @param scale_factor UINT32 scale factor (e.g. 3.6 to scale from m/s to km/h)
+ * @param value UINT32 value to be scaled
+ * @return scaled UINT32 value
+ */
 UINT32 scaling_uint32(UINT32 scale_factor, UINT32 value){
     return scale_factor * value;
 }
 
+/**
+ *
+ * @param scale_factor FLOAT64 scale factor (e.g. 3.6 to scale from m/s to km/h)
+ * @param value FLOAT64 value to be scaled
+ * @return scaled FLOAT64 value
+ */
 FLOAT64 scaling_float64(FLOAT64 scale_factor, FLOAT64 value){
     return scale_factor * value;
 }
 
+/**
+ *
+ * @param offset SINT32 offset to value
+ * @param value SINT32
+ * @return SINT32
+ */
 SINT32 offset_sin32(SINT32 offset, SINT32 value){
     return value + offset;
 }
 
+/**
+ *
+ * @param offset SINT32 offset to value
+ * @param value UINT32 value
+ * @return if the result is bigger than 0 the result will be returned otherwise 0 will be returned
+ */
 UINT32 offset_uint32(SINT32 offset, UINT32 value){
     SINT32 result = 0;
     result = value + offset;
@@ -44,6 +97,12 @@ UINT32 offset_uint32(SINT32 offset, UINT32 value){
     }
 }
 
+/**
+ *
+ * @param offset FLOAT64 offset to value
+ * @param value FLOAT64 value
+ * @return
+ */
 FLOAT64 offset_float64(FLOAT64 offset, FLOAT64 value){
     return value + offset;
 }
